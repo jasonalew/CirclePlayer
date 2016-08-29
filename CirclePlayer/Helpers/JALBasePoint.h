16cc18d7c1841fa4bc112f13beab07ea957517d5
Jason Lew <jason.a.lew@gmail.com>
2016-08-28 23:33:30 -0700

    initial commit

---
+++ CirclePlayer/Helpers/JALBasePoint.h
@@ -0,0 +1,14 @@
+//
+//  JALBasePoint.h
+//  CirclePlayer
+//
+//  Created by Jason Lew on 8/28/16.
+//  Copyright © 2016 Jason Lew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JALBasePoint : NSObject
+@property(nonatomic) CGPoint basePoint;
+@property(nonatomic) CGPoint destinationPoint;
+@end
---
+++ CirclePlayer/Helpers/JALBasePoint.m
@@ -0,0 +1,13 @@
+//
+//  JALBasePoint.m
+//  CirclePlayer
+//
+//  Created by Jason Lew on 8/28/16.
+//  Copyright © 2016 Jason Lew. All rights reserved.
+//
+
+#import "JALBasePoint.h"
+
+@implementation JALBasePoint
+
+@end
