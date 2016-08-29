2fb15cbbde1af711d9626bab289866caad57a21b
Jason Lew <jason.a.lew@gmail.com>
2016-08-29 14:01:26 -0700

    Formatting clean up

--- CirclePlayer/Helpers/JALPoint.h
+++ CirclePlayer/Helpers/JALPoint.h
@@ -11,12 +11,17 @@
 @interface JALPoint : NSObject
 
 + (CGFloat)dotProduct:(CGPoint)point1 point2:(CGPoint)point2;
-+ (CGFloat)squareLength:(CGPoint) point;
-+ (CGFloat)pointLength:(CGPoint) point;
-+ (CGPoint)add:(CGPoint) point1 point2:(CGPoint) point2;
-+ (CGPoint)subtract:(CGPoint) point1 point2:(CGPoint) point2;
-+ (CGPoint)multiply:(CGPoint) point1 value: (CGFloat) value;
-+ (CGPoint)pointOnCircleWithCenter:(CGPoint) center radius:(CGFloat) radius angle:(CGFloat) angle offset:(CGFloat) offset;
-+ (CGFloat)degreesToRadians:(CGFloat) degrees;
++ (CGFloat)squareLength:(CGPoint)point;
++ (CGFloat)pointLength:(CGPoint)point;
++ (CGPoint)add:(CGPoint)point1 point2:(CGPoint)point2;
++ (CGPoint)subtract:(CGPoint)point1 point2:(CGPoint)point2;
++ (CGPoint)multiply:(CGPoint)point1 value: (CGFloat)value;
++ (CGPoint)pointOnCircleWithCenter:(CGPoint)center
+                            radius:(CGFloat)radius
+                             angle:(CGFloat)angle
+                            offset:(CGFloat)offset;
++ (CGPoint)randomPointInCircleWithCenter:(CGPoint)center
+                                  radius:(CGFloat)radius;
++ (CGFloat)degreesToRadians:(CGFloat)degrees;
 
 @end
--- CirclePlayer/Helpers/JALPoint.m
+++ CirclePlayer/Helpers/JALPoint.m
@@ -14,33 +14,41 @@
     return point1.x * point2.x + point1.y * point2.y;
 }
 
-+ (CGFloat)squareLength:(CGPoint) point {
++ (CGFloat)squareLength:(CGPoint)point {
     return [JALPoint dotProduct:point point2:point];
 }
 
-+ (CGFloat)pointLength:(CGPoint) point {
++ (CGFloat)pointLength:(CGPoint)point {
     return sqrt([JALPoint squareLength:point]);
 }
 
-+ (CGPoint)add:(CGPoint) point1 point2:(CGPoint) point2 {
++ (CGPoint)add:(CGPoint)point1 point2:(CGPoint)point2 {
     return CGPointMake(point1.x + point2.x, point1.y + point2.y);
 }
 
-+ (CGPoint)subtract:(CGPoint) point1 point2:(CGPoint) point2 {
++ (CGPoint)subtract:(CGPoint)point1 point2:(CGPoint)point2 {
     return CGPointMake(point1.x - point2.x, point1.y - point2.y);
 }
 
-+ (CGPoint)multiply:(CGPoint) point1 value: (CGFloat) value {
++ (CGPoint)multiply:(CGPoint)point1 value:(CGFloat)value {
     return CGPointMake(point1.x * value, point1.y * value);
 }
 
-+ (CGPoint)pointOnCircleWithCenter:(CGPoint) center radius:(CGFloat) radius angle:(CGFloat) angle offset:(CGFloat) offset {
++ (CGPoint)pointOnCircleWithCenter:(CGPoint)center radius:(CGFloat)radius angle:(CGFloat)angle offset:(CGFloat)offset {
     CGFloat x = center.x + radius * cos([JALPoint degreesToRadians:angle + offset]);
     CGFloat y = center.y + radius * sin([JALPoint degreesToRadians:angle + offset]);
     return CGPointMake(x, y);
 }
 
-+ (CGFloat)degreesToRadians:(CGFloat) degrees {
++ (CGPoint)randomPointInCircleWithCenter:(CGPoint)center radius:(CGFloat)radius {
+    // Get random angle for point on circle
+    CGFloat randomAngle = arc4random_uniform(361);
+    // Get random value limited by radius
+    CGFloat randomRadius = arc4random_uniform((int)radius - 1) + 1;
+    return [JALPoint pointOnCircleWithCenter:center radius:randomRadius angle:randomAngle offset:0];
+}
+
++ (CGFloat)degreesToRadians:(CGFloat)degrees {
     return degrees * M_PI / 180;
 }
 
